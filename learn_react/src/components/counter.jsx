9f4d74705abdb4944915f77eaeabfe658defa8f4
omar saade <omarsaadeh11@gmail.com>
2022-04-18 00:59:34 +0300

    reatc

---
+++ learn_react/src/components/counter.jsx
@@ -0,0 +1,49 @@
+// imrc
+import React, { Component } from "react";
+
+//cc
+class Counter extends Component {
+  //state hye object
+  state = {
+    count: 0,
+    // imageUrl: "https://picsum.photos/200",
+  };
+
+  //   this is how can put a css style
+  //   styles = {
+  //     fontSize: 10,
+  //     fontWeight: "bold",
+  //   };
+
+  render() {
+    // let classes = this.getBadgeClasses();
+
+    //hayda jsx
+    return (
+      <div>
+        {/* this yaane Counter */}
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        {/* <span style={this.styles} className="badge badge-primary m-2"> */}
+        {/* <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    //Destructuring
+
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+    // return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
--- learn_react/src/index.js
+++ learn_react/src/index.js
@@ -3,3 +3,16 @@ import React from "react";
 
 import ReactDOM from "react-dom";
 
+import './index.css';
+
+import App from './App';
+
+//that's how we import bootstrap css in our application
+import 'bootstrap/dist/css/bootstrap.css';
+
+//cz counter is default export so we dont need the curly braces
+import Counter from './components/counter';
+
+
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
